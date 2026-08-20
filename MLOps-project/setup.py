96d50b62951f9f5ab485990ccb78838cbb6eb97a
dnyaneshwar chandane <dnyaneshwarchandane25@gmail.com>
2026-08-20 10:59:11 +0530

    initial project structure

---
+++ MLOps-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='these project is for practice profession way to do project',
+    author='Dnyaneshwar chandane',
+    license='',
+)
